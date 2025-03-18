f03cd940f0e01514bbac2f51afae3e5433cfcfc9
Revamth <rvrevamth@gmail.com>
2025-03-18 17:00:40 +0530

    Removed analytics

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAuQDfVzb8ix_IHtsTZmXoSfWbNauDhhTg",
   authDomain: "netflix-gpt-9f486.firebaseapp.com",
@@ -19,6 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-export const auth = getAuth();
+export const auth = getAuth(app);
